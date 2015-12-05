ad7dd3cf4e13244dbbd1f18963b69babfda313e9
ronaldo173 <my@mail>
2015-12-05 14:10:48 +0200

    synchronized keyword

---
+++ MultithreadingTutorial/src/tutorial/SynchronizationKeyword.java
@@ -0,0 +1,58 @@
+package tutorial;
+
+public class SynchronizationKeyword {
+	private  int count = 0;
+	
+
+	public static void main(String[] args) {
+		SynchronizationKeyword synchronizationKeyword = new SynchronizationKeyword();
+		synchronizationKeyword.doWork();
+	}
+
+	private synchronized void increment(){
+		count++;
+	}
+	public void doWork() {
+		// TODO Auto-generated method stub
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 10000; i++) {
+					increment();
+				}
+			}
+		}); 
+		
+		Thread t2 = new Thread (new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 10000; i++) {
+					increment();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		System.out.println("Count is: " + count);
+	}
+
+}
