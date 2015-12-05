7b8e04d3cf9151b11a6a98e9b1bc70afc1ee9608
ronaldo173 <my@mail>
2015-12-05 22:09:49 +0200

    simple Thread Pool

---
+++ MultithreadingTutorial/src/Thread_Pools/App.java
@@ -0,0 +1,52 @@
+package Thread_Pools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		for (int i = 0; i < 5; i++) {
+			executor.submit(new Processor(i));
+		}
+
+		executor.shutdown();
+		System.out.println("All tasks submited!");
+
+		try {
+			executor.awaitTermination(10000, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("All tasks completed!");
+	}
+}
+
+class Processor implements Runnable {
+	private int id;
+
+	public Processor(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Strarting: " + id);
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Complited: " + id);
+
+	}
+
+}
