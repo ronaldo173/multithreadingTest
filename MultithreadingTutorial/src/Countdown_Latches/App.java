bc3bfbaec663c90288b0fba5f6e3e730aa2a36bf
ronaldo173 <my@mail>
2015-12-06 00:09:45 +0200

    Countdown Latche added(защелка)

---
+++ MultithreadingTutorial/src/Countdown_Latches/App.java
@@ -0,0 +1,53 @@
+package Countdown_Latches;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+
+	public static void main(String[] args) {
+		CountDownLatch latch = new CountDownLatch(3);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+		for (int i = 0; i < 3; i++) {
+			executorService.submit(new Processors(latch));
+		}
+
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class Processors implements Runnable {
+
+	private CountDownLatch latch;
+
+	public Processors(CountDownLatch latch) {
+		// TODO Auto-generated constructor stub
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Started..");
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		latch.countDown();
+		System.out.println("..finished");
+
+	}
+
+}
