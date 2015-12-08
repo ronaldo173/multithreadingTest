a92602e3b6181f1b1a27295f789a5e1aa366d8b8
ronaldo173 <my@mail>
2015-12-08 09:00:00 +0200

    Re entrant locks

---
+++ MultithreadingTutorial/src/Re_entrant_lock/App.java
@@ -0,0 +1,53 @@
+package Re_entrant_lock;
+
+public class App {
+	public static void main(String[] args) {
+		Runner runner = new Runner();
+
+		System.out.println("hello..");
+
+		Thread threadFirst = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					runner.firstTHread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		Thread threadSecond = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					runner.secondThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		threadFirst.start();
+		threadSecond.start();
+
+		try {
+			threadFirst.join();
+			threadSecond.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		runner.finished();
+	}
+
+}
---
+++ MultithreadingTutorial/src/Re_entrant_lock/Runner.java
@@ -0,0 +1,50 @@
+package Re_entrant_lock;
+
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Runner {
+	private Lock lock = new ReentrantLock();
+	private Condition condition = lock.newCondition();
+	private int count = 0;
+
+	public void firstTHread() throws InterruptedException {
+		lock.lock();
+		condition.await();
+
+		try {
+			increment();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void secondThread() throws InterruptedException {
+		Thread.sleep(1000);
+		lock.lock();
+
+		System.out.println("Press return key..");
+		new Scanner(System.in).nextLine();
+		System.out.println("Fot return key!");
+
+		condition.signal();
+		try {
+			increment();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void finished() {
+		System.out.println("...finished..");
+		System.out.println("COunt is: " + count);
+	}
+
+	private void increment() {
+		for (int i = 0; i < 10000; i++) {
+			count++;
+		}
+	}
+}
